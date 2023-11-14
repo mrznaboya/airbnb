f16b7bf37f13d206a5eaefd642d44793c48fd897
mrznaboya <zuyax@yahoo.com>
2023-11-14 20:36:28 +0800

    Setup login authentication

--- app/(modals)/login.tsx
+++ app/(modals)/login.tsx
@@ -1,13 +1,127 @@
-import { View, Text, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 import React from "react";
 import { useWamUpBrowser } from "@/hooks/useWarmUpBrowser";
+import { defaultStyles } from "@/constants/Styles";
+import Colors from "@/constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
+import { useOAuth } from "@clerk/clerk-expo";
+import { useRouter } from "expo-router";
+
+enum Strategy {
+  Google = "oauth_google",
+  Apple = "oauth_apple",
+  Facebook = "oauth_facebook",
+}
 
 const Page = () => {
   useWamUpBrowser();
+  const router = useRouter();
+  const { startOAuthFlow: googleAuth } = useOAuth({ strategy: "oauth_google" });
+  const { startOAuthFlow: appleAuth } = useOAuth({ strategy: "oauth_apple" });
+  const { startOAuthFlow: facebookAuth } = useOAuth({
+    strategy: "oauth_facebook",
+  });
+
+  const onSelectAuth = async (strategy: Strategy) => {
+    const selectedAuth = {
+      [Strategy.Google]: googleAuth,
+      [Strategy.Apple]: appleAuth,
+      [Strategy.Facebook]: facebookAuth,
+    }[strategy];
+
+    try {
+      const { createdSessionId, setActive } = await selectedAuth();
+      console.log(
+        "🚀 ~ file: login.tsx:40 ~ onSelectAuth ~ createdSessionId:",
+        createdSessionId
+      );
+
+      if (createdSessionId) {
+        setActive!({ session: createdSessionId });
+        router.back();
+      }
+    } catch (err) {
+      console.log("OAuth error:", err);
+    }
+  };
 
   return (
     <View style={styles.container}>
-      <Text>Login</Text>
+      <TextInput
+        autoCapitalize="none"
+        placeholder="Email"
+        style={[defaultStyles.inputField, { marginBottom: 30 }]}
+      />
+      <TouchableOpacity style={defaultStyles.btn}>
+        <Text style={defaultStyles.btnText}>Continue</Text>
+      </TouchableOpacity>
+
+      <View style={styles.separatorView}>
+        <View
+          style={{
+            borderBottomColor: "#000",
+            borderBottomWidth: StyleSheet.hairlineWidth,
+            flex: 1,
+          }}
+        />
+        <Text style={styles.separator}>or</Text>
+        <View
+          style={{
+            flex: 1,
+            borderBottomColor: "#000",
+            borderBottomWidth: StyleSheet.hairlineWidth,
+          }}
+        />
+      </View>
+      <View style={{ gap: 20 }}>
+        <TouchableOpacity style={styles.btnOutline}>
+          <Ionicons
+            name="call-outline"
+            style={defaultStyles.btnIcon}
+            size={24}
+          />
+          <Text style={styles.btnOutlineText}>Continue with Phone</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.btnOutline}
+          onPress={() => onSelectAuth(Strategy.Apple)}
+        >
+          <Ionicons
+            name="md-logo-apple"
+            style={defaultStyles.btnIcon}
+            size={24}
+          />
+          <Text style={styles.btnOutlineText}>Continue with Apple</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.btnOutline}
+          onPress={() => onSelectAuth(Strategy.Google)}
+        >
+          <Ionicons
+            name="md-logo-google"
+            style={defaultStyles.btnIcon}
+            size={24}
+          />
+          <Text style={styles.btnOutlineText}>Continue with Google</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.btnOutline}
+          onPress={() => onSelectAuth(Strategy.Facebook)}
+        >
+          <Ionicons
+            name="md-logo-facebook"
+            style={defaultStyles.btnIcon}
+            size={24}
+          />
+          <Text style={styles.btnOutlineText}>Continue with Facebook</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -17,6 +131,32 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#FFF",
     padding: 26,
+  },
+  separatorView: {
+    flexDirection: "row",
+    gap: 10,
+    alignItems: "center",
+    marginVertical: 30,
+  },
+  separator: {
+    fontFamily: "mon-sb",
+    color: Colors.grey,
+  },
+  btnOutline: {
+    backgroundColor: "#FFF",
+    borderWidth: 1,
+    borderColor: Colors.grey,
+    borderRadius: 8,
+    paddingHorizontal: 10,
+    height: 50,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  btnOutlineText: {
+    fontFamily: "mon-sb",
+    fontSize: 16,
+    color: "#000",
   },
 });
 
