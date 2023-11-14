056ae401d171384d35cd6b8ffd7bf5a0eec7115d
mrznaboya <zuyax@yahoo.com>
2023-11-14 15:18:17 +0800

    login touch-ups, added logout and login in profile

--- app/(modals)/login.tsx
+++ app/(modals)/login.tsx
@@ -1,12 +1,23 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
+import { useWamUpBrowser } from "@/hooks/useWarmUpBrowser";
 
 const Page = () => {
+  useWamUpBrowser();
+
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Login</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFF",
+    padding: 26,
+  },
+});
 
 export default Page;
