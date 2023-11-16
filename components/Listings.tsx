10878993a662dd3138ebf82faa5e87b9fcff2e1c
mrznaboya <zuyax@yahoo.com>
2023-11-16 20:44:30 +0800

    Updated Listings

--- components/Listings.tsx
+++ components/Listings.tsx
@@ -9,6 +9,7 @@ import {
 } from "react-native";
 import React, { useEffect, useRef, useState } from "react";
 import { Link } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
 
 interface Props {
   listings: any[];
@@ -33,7 +34,36 @@ const Listings = ({ listings: items, category }: Props) => {
       <TouchableOpacity>
         <View style={styles.listing}>
           <Image source={{ uri: item.medium_url }} style={styles.image} />
-          <TouchableOpacity></TouchableOpacity>
+          <TouchableOpacity
+            style={{ position: "absolute", right: 30, top: 30 }}
+          >
+            <Ionicons name="heart-outline" size={24} color={"#000"} />
+          </TouchableOpacity>
+          <View
+            style={{ flexDirection: "row", justifyContent: "space-between" }}
+          >
+            <Text
+              style={{
+                fontSize: 16,
+                fontFamily: "mon-sb",
+              }}
+            >
+              {item.name}
+            </Text>
+            <View style={{ flexDirection: "row", gap: 4 }}>
+              <Ionicons name="star" size={16} />
+              <Text style={{ fontFamily: "mon-sb" }}>
+                {item.review_scores_rating / 20}
+              </Text>
+            </View>
+          </View>
+
+          <Text style={{ fontFamily: "mon-r" }}>{item.room_type}</Text>
+
+          <View style={{ flexDirection: "row", gap: 4 }}>
+            <Text style={{ fontFamily: "mon-sb" }}>$ {item.price}</Text>
+            <Text style={{ fontFamily: "mon-r" }}>night</Text>
+          </View>
         </View>
       </TouchableOpacity>
     </Link>
@@ -53,6 +83,8 @@ const Listings = ({ listings: items, category }: Props) => {
 const styles = StyleSheet.create({
   listing: {
     padding: 16,
+    gap: 10,
+    marginVertical: 16,
   },
   image: {
     width: "100%",
