2ff4b1b97af9cf0459ef5b3ecf4d46008d281368
mrznaboya <zuyax@yahoo.com>
2023-11-16 19:51:44 +0800

    Added listing and updated Listing to show images

--- components/Listings.tsx
+++ components/Listings.tsx
@@ -1,21 +1,64 @@
-import { View, Text } from "react-native";
-import React, { useEffect } from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  ListRenderItem,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+} from "react-native";
+import React, { useEffect, useRef, useState } from "react";
+import { Link } from "expo-router";
 
 interface Props {
   listings: any[];
   category: string;
 }
 
-const Listings = ({ listings, category }: Props) => {
+const Listings = ({ listings: items, category }: Props) => {
+  const [loading, setLoading] = useState(false);
+  const listRef = useRef<FlatList>(null);
+
   useEffect(() => {
-    console.log("RELOAD LISTINGS: ", listings.length);
+    console.log("RELOAD LISTINGS: ", items.length);
+    setLoading(true);
+
+    setTimeout(() => {
+      setLoading(false);
+    }, 200);
   }, [category]);
+
+  const renderRow: ListRenderItem<Listing> = ({ item }) => (
+    <Link href={`/listing/${item.id}`} asChild>
+      <TouchableOpacity>
+        <View style={styles.listing}>
+          <Image source={{ uri: item.medium_url }} style={styles.image} />
+          <TouchableOpacity></TouchableOpacity>
+        </View>
+      </TouchableOpacity>
+    </Link>
+  );
 
   return (
     <View>
-      <Text>Listings</Text>
+      <FlatList
+        renderItem={renderRow}
+        ref={listRef}
+        data={loading ? [] : items}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  listing: {
+    padding: 16,
+  },
+  image: {
+    width: "100%",
+    height: 300,
+    borderRadius: 10,
+  },
+});
 
 export default Listings;
